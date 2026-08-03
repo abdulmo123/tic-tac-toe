1f5b48dfb668bcc17dd488faf19ee49ef5e71d48
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-08-03 14:53:43 -0500

    trying to use factory functions + iife way

--- game.js
+++ game.js
@@ -1,51 +1,115 @@
-// TODO: first work on logic for placing items in a cell when you click it
-
-// 2 player objects created inside of a startGame() function?
-
-// player object - name, symbol (X or O), isTurn?
-
-// TODO: try the module approach 
-// player object
-function createPlayer(name, marker) {
+const createPlayer = (name, marker) => {
     return { name, marker }
 }
 
-// game board object
-let gameBoard = (function () {
-    let board = [];
+let Gameboard = (function () {
+    let board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+    const getGameboard = () => board;
 
-    const getGameBoard = () => board;
-
-    const setMarker = (row, col, marker) => {
-        if (board[row][col] === "") {
-            board[row][col] = marker;
-            return true;
-        }
-
-        return false;
-    };
-
-    const resetBoard = () => {
-        board.fill("");
+    const setBoardMarker = (row, col, activePlayer) => {
+        board[row][col] = activePlayer.marker;
     }
 
-
-    return { getGameBoard, setMarker, resetBoard }
+    return { getGameboard, setBoardMarker }
 })();
 
-// game state object
-let gameController = (() => {
-    
+let GameController = (function () {
+    const player1 = createPlayer("John", "X");
+    const player2 = createPlayer("Jim", "O");
+    const gamePlayers = [player1, player2];
+    const gameBoard = Gameboard.getGameboard();
+
+    let activePlayer = player1;
+    let winner = {};
+
+    const setMarker = (row, col) => {
+        Gameboard.setBoardMarker(row, col, activePlayer);
+        activePlayer === player1 ? activePlayer = player2 : activePlayer = player1;
+        DisplayController.render();
+        checkWinner();
+    }
+
+    const resetGame = () => {
+        let gameBoard = Gameboard.getGameboard();
+        for (let r = 0; r < gameBoard.length; r++) {
+            for (let c = 0; c < gameBoard.length; c++) {
+                gameBoard[r][c] = "";
+            }
+        }
+        activePlayer = player1;
+        DisplayController.render();
+    }
+
+    // check who won game
+    function checkWinner() {
+        if ((gameBoard[0][0] !== '' &&
+            gameBoard[0][0] === gameBoard[1][1] &&
+            gameBoard[1][1] === gameBoard[2][2]) ||
+            (gameBoard[0][0] !== '' &&
+                gameBoard[0][0] === gameBoard[0][1] &&
+                gameBoard[0][1] === gameBoard[0][2])) {
+            const winningMarker = gameBoard[0][0];
+
+            const winner = winningMarker ? gamePlayers.find((player) => player.marker === winningMarker) : null;
+            console.log('winner!', winner);
+        } else {
+            // check if the board is full to declare draw
+            if (checkDraw()) {
+                console.log('GAME HAS ENDED IN A DRAW');
+            }
+        }
+    }
+
+    function checkDraw() {
+        const blank = "";
+        for (let r = 0; r < gameBoard.length; r++) {
+            for (let c = 0; c < gameBoard.length; c++) {
+                if (gameBoard[r][c] !== blank) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    return { setMarker, resetGame };
 })();
 
 
-// display controller
-let displayController = (() => {
+let DisplayController = (function () {
+    // place marker
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('click', (e) => {
+            GameController.setMarker(cell.dataset.row, cell.dataset.col);
+            cell.disabled = true;
+        });
+    });
 
+    // reset game 
+    const resetBtn = document.querySelector('.reset-btn');
+    resetBtn.addEventListener('click', (e) => {
+        GameController.resetGame();
+        cells.forEach((cell) => {
+            cell.disabled = false;
+        })
+    });
+
+
+    // render display on screen - happens after each turn
+    const render = () => {
+        const gameBoard = Gameboard.getGameboard();
+        for (let r = 0; r < gameBoard.length; r++) {
+            for (let c = 0; c < gameBoard.length; c++) {
+                const cell = document.querySelector(`button[data-row="${r}"][data-col="${c}"]`);
+                cell.textContent = gameBoard[r][c];
+            }
+        }
+    }
+
+    return { render }
 })();
-const player1 = createPlayer("Player 1", "X", true);
-const player2 = createPlayer("Player 2", "O", false);
-
-
-console.log('player 1 ...', player1);
-console.log('player 2 ...', player2);
